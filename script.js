c429da24d26f090479032002fc6a0a94f439da1a
Joseph27Tan <joseph27tan2004@gmail.com>
2025-03-31 00:29:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.getElementById("changeStyle").addEventListener("click", function() {
+    // Add your code here to change the table style.  For example, you might change the background color or add a border.
+    // Example:
+    // document.querySelector("table").style.backgroundColor = "lightblue";
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
